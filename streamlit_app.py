d25a2bc68806f9cd97a76b06c1f5bb0231f42ccf
Abigail99216 <chenshuyi216@gmail.com>
2024-07-28 14:43:00 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from langchain_openai import ChatZhipuAI
+from langchain_community.chat_models import ChatZhipuAI
 import os
 from langchain_core.output_parsers import StrOutputParser
 from langchain.prompts import PromptTemplate
