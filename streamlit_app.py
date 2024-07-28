adb00b8a0c96619d65bbd58fbfef1a621d0a08f7
Abigail99216 <chenshuyi216@gmail.com>
2024-07-28 18:58:52 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ zhipuai_api_key = os.environ['ZHIPUAI_API_KEY']
 
 
 def generate_response(input_text, zhipuai_api_key):
-    llm = ZhipuAI(temperature=0.7, zhipuai_api_key=zhipuai_api_key)
+    llm = ZhipuAI(zhipuai_api_key=zhipuai_api_key)
     output = llm.invoke(input_text)
     output_parser = StrOutputParser()
     output = output_parser.invoke(output)
